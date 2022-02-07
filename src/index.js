a228214943d633b40286343a995bd7b86cad0b78
jubi020 <jubikumari020@gmail.com>
2022-02-07 12:22:03 +0530

    installed a new dependency

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css';
+// import 'dotenv/config';
+
+console.log(process.env);
 
 ReactDOM.render(
   <React.StrictMode>
